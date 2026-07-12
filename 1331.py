21b36b0e733a45425fe14cbc15d925a2bce3057f
ak <aksharasajeevanm@gmail.com>
2026-07-12 13:39:15 +0800

    Implement array rank transformation in 1331.py

---
+++ 1331.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        sorted_unique = sorted(list(set(arr)))
+
+        rank_map = {}
+        for rank, num in enumerate(sorted_unique, 1):
+            rank_map[num] =  rank
+
+        return [rank_map[num] for num in arr]
