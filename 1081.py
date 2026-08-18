ef5aac89d8b1558f9e657b3cfcd081412c24d095
ak <aksharasajeevanm@gmail.com>
2026-08-18 22:04:12 +0530

    Improve code formatting in smallestSubsequence method
    Added whitespace for better readability.

--- 1081.py
+++ 1081.py
@@ -2,6 +2,9 @@ class Solution:
     def smallestSubsequence(self, s: str) -> str:
         last_occurrence = {char: i for i, char in enumerate(s)}
 
+
+        
+
         stack = []
         seen = set()
 
