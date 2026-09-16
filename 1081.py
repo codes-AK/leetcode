c176ff5608f69313ca0b6e5563a62aaf09fa4f84
ak <aksharasajeevanm@gmail.com>
2026-09-16 21:07:16 +0530

    Improve stack character management in 1081.py
    Refactor character handling in the stack logic.

--- 1081.py
+++ 1081.py
@@ -9,6 +9,10 @@ class Solution:
             if char in seen:
                 continue
 
+
+
+            
+
         
             while stack and stack[-1] > char and last_occurrence[stack[-1]] > i:
                 removed_char = stack.pop()
